1da0580c740dfe3ad7b7628d354aaa5980115275
Christian Laustsen <ckl@codetalk.io>
2017-08-20 15:06:42 +0200

    Setup package skeleton

---
+++ spec/atom-haskell-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomHaskellView from '../lib/atom-haskell-view';
+
+describe('AtomHaskellView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
